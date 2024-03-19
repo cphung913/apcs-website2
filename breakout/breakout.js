78396cfea4a8c7a7c1649efe9ffcbf85cb590dfb
Chase Phung <69651488+cphung913@users.noreply.github.com>
2024-03-19 17:18:19 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- breakout/breakout.js
+++ breakout/breakout.js
@@ -7,3 +7,15 @@ const ctx = canvas.getContext('2d');
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
 closeBtn.addEventListener('click', () => rules.classList.remove('show'));
 
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+}
+
+function drawBall() {
+    
+}
